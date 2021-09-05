0173e242a7a3d25ce59787a6d406846c01cc3ab9
ClaudiaThalita <claudiathalita@hotmail.com>
2021-09-04 23:20:04 -0300

    feature: aula26

---
+++ aula26.py
@@ -0,0 +1,10 @@
+print(True)
+print(False)
+print(1.2_ 1)
+print('Aqui eu falo na minha lingua')
+print('Você é' + 3 * 'muito' + 'legal!')
+#print(3 + '3') ->  ambiguidade
+
+print([1,2,3]) #lista   
+print({'nome:' Pedro, 'idade': 22}) #dicionário 
+print (None)
